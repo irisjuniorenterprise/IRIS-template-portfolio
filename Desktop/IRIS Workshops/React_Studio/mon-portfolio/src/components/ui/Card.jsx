7d5d558ee93663f2745d0a9726db058e2df38743
MissaouiYassine1 <yassine.missaoui@enis.tn>
2026-08-02 22:58:04 +0200

    feat: ajouter la carte

---
+++ Desktop/IRIS Workshops/React_Studio/mon-portfolio/src/components/ui/Card.jsx
@@ -0,0 +1,18 @@
+function Card({ title, description, image, children }) {
+    return (
+        <div style={{
+            background: 'white',
+            borderRadius: '12px',
+            padding: '20px',
+            boxShadow: '0 4px 12px rgba(0,0,0,0.08)',
+            border: '1px solid #e5e7eb',
+            maxWidth: '300px'
+            }}>
+            {image && <img src={image} alt={title} />}
+            <h3>{title}</h3>
+            <p>{description}</p>
+            {children}
+        </div>
+    );
+}
+export default Card;
