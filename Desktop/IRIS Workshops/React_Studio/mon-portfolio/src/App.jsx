2007f2579c8a83a1fc8043ec49434216866e7409
MissaouiYassine1 <yassine.missaoui@enis.tn>
2026-08-02 22:33:47 +0200

    feat: ajouter la section héro à la page principale

--- Desktop/IRIS Workshops/React_Studio/mon-portfolio/src/App.jsx
+++ Desktop/IRIS Workshops/React_Studio/mon-portfolio/src/App.jsx
@@ -1,8 +1,10 @@
 import Header from './components/sections/Header.jsx';
+import Hero from './components/sections/Hero.jsx';
 function App() {
   return (
   <div>
     <Header />
+    <Hero />
     <main >Contenu principal ....</main>
   </div>
   );
