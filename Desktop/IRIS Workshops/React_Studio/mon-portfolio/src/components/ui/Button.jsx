1dae7089a26176fc334e64200464a0ee624cd536
MissaouiYassine1 <yassine.missaoui@enis.tn>
2026-08-02 22:32:09 +0200

    feat: add the custom button

---
+++ Desktop/IRIS Workshops/React_Studio/mon-portfolio/src/components/ui/Button.jsx
@@ -0,0 +1,22 @@
+function Button({ children, variant = 'primary', onClick }) {
+    const styles = {
+    primary:   { background: '#1a3969', color: 'white' },
+    secondary: { background: '#ff6633', color: 'white' },
+    outline:   { background: 'transparent', border: '2px solid #1a3969' }
+    };
+    return (
+        <button
+            onClick={onClick}
+            style={{
+            padding: '10px 24px',
+            borderRadius: '8px',
+            fontWeight: 'bold',
+            cursor: 'pointer',
+            ...styles[variant]
+            }}
+            >
+            {children}
+        </button>
+    );
+}
+export default Button;
